344294127056cbd805628587e70fe34470c70e5e
jhonatancasale <casale.jhon@gmail.com>
2018-07-10 23:17:34 -0300

    Use DEBUG True again

--- src/scheduling_system/scheduling_system/settings.py
+++ src/scheduling_system/scheduling_system/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default=secret_key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
